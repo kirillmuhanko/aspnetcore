82c7c23f877da82c0244dfc340f1ea24211079f3
kirillmuhanko <79082614+kirillmuhanko@users.noreply.github.com>
2022-07-24 13:11:23 +0300

    feat: Web optimizer

---
+++ samples/WebApplication1/WebApplication1/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using NUglify.Css;
+using NUglify.JavaScript;
+
+namespace WebApplication1.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddWebOptimizer(this IServiceCollection collection, IWebHostEnvironment environment)
+    {
+        var isDevelopment = environment.IsDevelopment();
+        var cssSettings = isDevelopment ? CssSettings.Pretty() : new CssSettings();
+        var codeSettings = isDevelopment ? CodeSettings.Pretty() : new CodeSettings();
+
+        collection.AddWebOptimizer(assetPipeline =>
+        {
+            assetPipeline.AddCssBundle("/css/site.css", cssSettings, "/css/**/*.css");
+            assetPipeline.AddJavaScriptBundle("/js/site.js", codeSettings, "/js/**/*.js");
+        });
+    }
+}
